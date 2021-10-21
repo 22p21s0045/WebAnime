172f54cfa35ad27b7f439225baca4e1b8568c116
aa aa <gtaiv20306@gmail.com>
2021-10-21 14:51:33 +0700

    create all box

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -10,51 +10,62 @@ export default function Box() {
         <div>
             <Container>
                 <Row>
-                <Col/>
+                    <Col>     <Card style={{ width: '18rem', height: '18rem' }}>
 
-                    <Card style={{ width: '18rem', height: '18rem' }}
-                    >
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
                     </Card>
-                <Col/>
-                <Col/>
 
-                    <Card style={{ width: '18rem', height: '18rem' }}
-                    >
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                    </Card>
-                <Col/>
-                <Col/>
+                    </Col>
+                    <Col>    <Card style={{ width: '18rem', height: '18rem' }}>
 
-                    <Card style={{ width: '18rem', height: '18rem' }}
-                    >
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>   </Col>
+                </Row>
+                <Row>
+                    <Col>     <Card style={{ width: '18rem', height: '18rem' }}>
+
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
                     </Card>
-                <Col/>
+
+                    </Col>
+                    <Col>    <Card style={{ width: '18rem', height: '18rem' }}>
+
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>   </Col>
+
+
 
                 </Row>
             </Container>
