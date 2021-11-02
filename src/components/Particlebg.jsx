3b5945f920041cb6b6457da3bfd61909bfa2b1ad
22p21s0045-อัครวิทย์ <71715345+22p21s0045@users.noreply.github.com>
2021-11-02 14:35:48 +0700

    particle is full screen now

--- src/components/Particlebg.jsx
+++ src/components/Particlebg.jsx
@@ -2,11 +2,29 @@ import React from 'react'
 import Particles from 'react-particles-js';
 import ParticleConfig from "./particle-config"
 
-
 export default function Particlebg() {
   return (
-    
-    <Particles  params={ParticleConfig}></Particles>
-    
+    <div id="particles-js">
+   <Particles
+  params={{
+    particles: {
+      color: {
+        value: "#186A3B"
+      },
+      line_linked: {
+        color: {
+          value: "#000000"
+        }
+      },
+      number: {
+        value: 50
+      },
+      size: {
+        value: 3
+      }
+    }
+  }}
+/>
+    </div>
   )
 }
