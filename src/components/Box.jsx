cbb364d6a071a4bf99c70e12f6e7856a38e141ba
22p21s0045-อัครวิทย์ <71715345+22p21s0045@users.noreply.github.com>
2021-11-05 16:34:23 +0700

    change color box success

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -25,7 +25,7 @@ export default function Box() {
                         <Card style={{ width: '18rem', height: '18rem' }}>
 
                             <Card.Img variant="top" src={data.data?.top[0].image_url} style={{ width: '18rem', height: '18rem' }} />
-                            <Card.Body style={{ backgroundColor: '#151514' }}>
+                            <Card.Body style={{ backgroundColor: '#FAEEE0' }}>
                                 <Card.Title>1.{data.data?.top[0].title}</Card.Title>
                                 <Card.Text>
                                     Some quick example text to build on the card title and make up the bulk of
@@ -40,7 +40,7 @@ export default function Box() {
                     <Col style={{ paddingRight: 50 }} classname ='boxs'  md={6} lg ={4} >    <Card style={{ width: '18rem', height: '18rem' }}>
 
                         <Card.Img variant="top" src={data.data?.top[1].image_url} style={{ width: '18rem', height: '18rem' }} />
-                        <Card.Body>
+                        <Card.Body style={{ backgroundColor: '#FAEEE0' }}>
                             <Card.Title>2.{data.data?.top[1].title}</Card.Title>
                             <Card.Text>
                                 Some quick example text to build on the card title and make up the bulk of
@@ -53,7 +53,7 @@ export default function Box() {
                     <Col style={{  paddingLeft: 0 }}classname ='boxs'  md={6} lg ={4}>    <Card style={{ width: '18rem', height: '18rem' }}>
 
                         <Card.Img variant="top" src={data.data?.top[2].image_url} style={{ width: '18rem', height: '18rem' }} />
-                        <Card.Body>
+                        <Card.Body style={{ backgroundColor: '#FAEEE0' }}>
                             <Card.Title>3.{data.data?.top[2].title}</Card.Title>
                             <Card.Text>
                                 Some quick example text to build on the card title and make up the bulk of
@@ -71,7 +71,7 @@ export default function Box() {
                     <Col classname ='boxs'  md={6} lg ={4}>     <Card style={{ width: '18rem', height: '18rem' }}>
 
                         <Card.Img variant="top" src={data.data?.top[3].image_url} style={{ width: '18rem', height: '18rem' }} />
-                        <Card.Body>
+                        <Card.Body style={{ backgroundColor: '#FAEEE0' }}>
                             <Card.Title>4.{data.data?.top[3].title}</Card.Title>
                             <Card.Text>
                                 Some quick example text to build on the card title and make up the bulk of
@@ -85,7 +85,7 @@ export default function Box() {
                     <Col  classname ='boxs'  md={6} lg ={4}>    <Card style={{ width: '18rem', height: '18rem' }}>
 
                         <Card.Img variant="top" src={data.data?.top[4].image_url} style={{ width: '18rem', height: '18rem' }} />
-                        <Card.Body>
+                        <Card.Body style={{ backgroundColor: '#FAEEE0' }}>
                             <Card.Title>5.{data.data?.top[4].title}</Card.Title>
                             <Card.Text>
                                 Some quick example text to build on the card title and make up the bulk of
@@ -99,7 +99,7 @@ export default function Box() {
                     <Col classname ='boxs'  md={6} lg ={4}>    <Card style={{ width: '18rem', height: '18rem' }}>
 
                         <Card.Img variant="top" src={data.data?.top[5].image_url} style={{ width: '18rem', height: '18rem' }} />
-                        <Card.Body>
+                        <Card.Body style={{ backgroundColor: '#FAEEE0' }}>
                             <Card.Title>6.{data.data?.top[5].title}</Card.Title>
                             <Card.Text>
                                 Some quick example text to build on the card title and make up the bulk of
@@ -125,7 +125,7 @@ export default function Box() {
                     <Col >     <Card style={{ width: '18rem', height: '18rem' }}>
 
                         <Card.Img variant="top" src={data.data?.top[6].image_url} style={{ width: '18rem', height: '18rem' }} />
-                        <Card.Body>
+                        <Card.Body style={{ backgroundColor: '#FAEEE0' }}>
                             <Card.Title>7.{data.data?.top[6].title}</Card.Title>
                             <Card.Text>
                                 Some quick example text to build on the card title and make up the bulk of
@@ -139,7 +139,7 @@ export default function Box() {
                     <Col classname ='boxs'>    <Card style={{ width: '18rem', height: '18rem' }}>
 
                         <Card.Img variant="top" src={data.data?.top[7].image_url} style={{ width: '18rem', height: '18rem' }} />
-                        <Card.Body>
+                        <Card.Body style={{ backgroundColor: '#FAEEE0' }}>
                             <Card.Title>8.{data.data?.top[7].title}</Card.Title>
                             <Card.Text>
                                 Some quick example text to build on the card title and make up the bulk of
@@ -153,7 +153,7 @@ export default function Box() {
                     <Col classname ='boxs'>    <Card style={{ width: '18rem', height: '18rem' }}>
 
                         <Card.Img variant="top" src={data.data?.top[8].image_url} style={{ width: '18rem', height: '18rem' }} />
-                        <Card.Body>
+                        <Card.Body style={{ backgroundColor: '#FAEEE0' }}>
                             <Card.Title>9.{data.data?.top[8].title}</Card.Title>
                             <Card.Text>
                                 Some quick example text to build on the card title and make up the bulk of
@@ -179,7 +179,7 @@ export default function Box() {
                     <Col  classname ='boxs'>     <Card style={{ width: '18rem', height: '18rem' }}>
 
                         <Card.Img variant="top" src={data.data?.top[9].image_url} style={{ width: '18rem', height: '18rem' }} />
-                        <Card.Body>
+                        <Card.Body style={{ backgroundColor: '#FAEEE0' }}>
                             <Card.Title>10.{data.data?.top[9].title}</Card.Title>
                             <Card.Text>
                                 Some quick example text to build on the card title and make up the bulk of
@@ -193,7 +193,7 @@ export default function Box() {
                     <Col classname ='boxs'>    <Card style={{ width: '18rem', height: '18rem' }}>
 
                         <Card.Img variant="top" src={data.data?.top[10].image_url} style={{ width: '18rem', height: '18rem' }} />
-                        <Card.Body>
+                        <Card.Body style={{ backgroundColor: '#FAEEE0' }}>
                             <Card.Title>11.{data.data?.top[10].title}</Card.Title>
                             <Card.Text>
                                 Some quick example text to build on the card title and make up the bulk of
@@ -207,7 +207,7 @@ export default function Box() {
                     <Col classname ='boxs'>    <Card style={{ width: '18rem', height: '18rem' }}>
 
                         <Card.Img variant="top" src={data.data?.top[11].image_url} style={{ width: '18rem', height: '18rem' }} />
-                        <Card.Body>
+                        <Card.Body style={{ backgroundColor: '#FAEEE0' }}>
                             <Card.Title>12.{data.data?.top[11].title}</Card.Title>
                             <Card.Text>
                                 Some quick example text to build on the card title and make up the bulk of
@@ -233,7 +233,7 @@ export default function Box() {
                     <Col >     <Card style={{ width: '18rem', height: '18rem' }}>
 
                         <Card.Img variant="top" src={data.data?.top[12].image_url} style={{ width: '18rem', height: '18rem' }} />
-                        <Card.Body>
+                        <Card.Body style={{ backgroundColor: '#FAEEE0' }}>
                             <Card.Title>13.{data.data?.top[12].title}</Card.Title>
                             <Card.Text>
                                 Some quick example text to build on the card title and make up the bulk of
@@ -247,7 +247,7 @@ export default function Box() {
                     <Col >    <Card style={{ width: '18rem', height: '18rem' }}>
 
                         <Card.Img variant="top" src={data.data?.top[13].image_url} style={{ width: '18rem', height: '18rem' }} />
-                        <Card.Body>
+                        <Card.Body style={{ backgroundColor: '#FAEEE0' }}>
                             <Card.Title>14.{data.data?.top[13].title}</Card.Title>
                             <Card.Text>
                                 Some quick example text to build on the card title and make up the bulk of
@@ -261,7 +261,7 @@ export default function Box() {
                     <Col >    <Card style={{ width: '18rem', height: '18rem' }}>
 
                         <Card.Img variant="top" src={data.data?.top[14].image_url} style={{ width: '18rem', height: '18rem' }} />
-                        <Card.Body>
+                        <Card.Body style={{ backgroundColor: '#FAEEE0' }}>
                             <Card.Title>15.{data.data?.top[14].title}</Card.Title>
                             <Card.Text>
                                 Some quick example text to build on the card title and make up the bulk of
